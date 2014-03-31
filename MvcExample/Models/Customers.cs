17619afa1bbb727470de30f0ba6fbd74d47822f5
Dany Paredes <danywalls4@gmail.com>
2014-03-31 22:31:28 +0200

    First version of validate
    First version using jquery validate

---
+++ MvcExample/Models/Customers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcExample.Models
+{
+    public class Customers
+    {
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public string country { get; set; }
+        public int age { get; set; }
+    }
+}
